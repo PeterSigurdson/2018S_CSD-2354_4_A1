06d1de55fe658fbe2cfdc4985a5cc2b6ee632866
Peter Sigurdson <peter@petersigurdson.net>
2018-06-18 06:28:48 -0400

    20180618a: Added MemoryMap study code

---
+++ MemoryMap_TestCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program1
+{
+    static void Main()
+    {
+        int count = 1;
+        int iteration = 0;
+
+        while (count < 10)
+        {
+            iteration += 1;
+            Console.WriteLine("iteration is {0}", iteration);
+
+            Console.WriteLine("Point A: count is {0}", count);
+            count += 1;
+
+            if (count > 5)
+            {
+                Console.WriteLine("I am in the If statement now...");
+                count += 2;
+            }
+            Console.WriteLine("Point B: count is {0}", count);
+        }
+
+        Console.WriteLine("the final value of count is {0}", count);
+
+        Console.ReadLine();
+    }
+}
